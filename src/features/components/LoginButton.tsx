61d18432d9ddc781858845b1ee36bb3053b4ba9b
asepsaepulr <asepsaepulr88@gmail.com>
2024-11-26 05:20:44 +0700

    nit: add image sample

--- src/features/components/LoginButton.tsx
+++ src/features/components/LoginButton.tsx
@@ -5,7 +5,7 @@ import {signIn} from "next-auth/react";
 
 export default function LoginButton() {
   const handleLogin = () => {
-    signIn("spotify", { callbackUrl: "http://localhost:3000" });
+    signIn("spotify", { callbackUrl: "/" });
   };
   return (
     <div>
--- src/features/components/LogoutButton.tsx
+++ src/features/components/LogoutButton.tsx
@@ -6,7 +6,7 @@ import { signOut } from 'next-auth/react';
 
 const LogoutButton = () => {
   const handeLogout = () => {
-    signOut({ callbackUrl: "http://localhost:3000/login" });
+    signOut({ callbackUrl: "/login" });
   };
   return (
     <DropdownMenuItem onClick={handeLogout}>Logout</DropdownMenuItem>
