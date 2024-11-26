e32b9f08654b30b93b72f2be7cd1ad9419a4672a
Asep Saepul Rohmat <57656151+asepsaepulr@users.noreply.github.com>
2024-11-26 11:30:02 +0700

    fix(auth): add scope

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify";
 
+const scope =
+  "user-read-recently-played user-read-playback-state user-top-read user-modify-playback-state user-read-currently-playing user-follow-read playlist-read-private user-read-email user-read-private user-library-read playlist-read-collaborative";
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
   session: {
     strategy: "jwt",
@@ -9,6 +12,9 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID as string,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+      authorization: {
+        params: { scope },
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
